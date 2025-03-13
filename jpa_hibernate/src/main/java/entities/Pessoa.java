967c0628b110af6c2101548a9a3489c1d94d94fd
saulocoelho <coelho.saulo@gmail.com>
2025-03-13 14:31:51 +0000

    update Pessoa.java

--- jpa_hibernate/src/main/java/entities/Pessoa.java
+++ jpa_hibernate/src/main/java/entities/Pessoa.java
@@ -1,17 +1,7 @@
 package entities;
 
-public package entities;
-
+import javax.persistence.*;
 import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "pessoa")
@@ -97,6 +87,4 @@ public class Pessoa {
     public void setDataNascimento(Date dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
-} {
-    
 }
